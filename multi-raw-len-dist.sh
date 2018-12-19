e65a1c5a0755e993fd11feb0180d715c860f71fc
Heather Milne <heatherjmilne@gmail.com>
2018-12-19 11:27:24 -0800

    New version of raw-len-dist

---
+++ multi-raw-len-dist.sh
@@ -0,0 +1,26 @@
+#!bin/bash
+
+echo "Running..."
+for filename in ./*R2_001.fastq.gz; do
+	echo $filename
+	#Identify and unzip file
+	gunzip ${filename}
+	echo "Unzipped file"
+	prefix=$(echo $filename | awk -F'[_]' '{print $1}')
+	echo Prefix: $prefix
+	outfile=$prefix-rawlength.txt
+	#Iterate over sequence length before adapter sequence (R2)
+	LEN=0
+	while [ "$LEN" -lt 71 ]; do
+		#echo Length: $LEN
+		VAR=$(grep "^[A-Z]\{$LEN\}AGATCGG" ${prefix}*R2_001.fastq | wc -l)
+		#echo Occurences: $VAR
+		#Write values to CSV
+		echo -n "$LEN	$VAR" >> $outfile
+		echo "" >> $outfile
+		let LEN=LEN+1
+	done
+
+	#Close file
+	gzip ${prefix}*R2_001.fastq
+done
